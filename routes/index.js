c075c51e16d37176932486f749137dcbf3a323ed
Tommy Warnecke <t.warnecke@novacapta.de>
2017-04-21 12:07:33 +0200

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html', { title: 'MyPersonalPage' });
+  res.render('index.html', { title: 'TommyWarnecke' });
 });
 
 module.exports = router;
